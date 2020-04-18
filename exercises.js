40e55d761be9011d64c444c901b9199c63e900a8
Nicolás Ortolá <nicolas.ortola@bpmx.com.ar>
2020-04-18 13:54:44 -0300

    Init exercises repository

---
+++ exercises.js
@@ -0,0 +1,29 @@
+
+
+const catalog = [
+    {
+        name: 'monkey fart',
+        stock: 5
+    },
+    {
+        name: 'tribeca',
+        stock: 10
+    },
+    {
+        name: 'tanaco',
+        stock: 2
+    }
+]
+
+/* Desarrollar la función addToCatalog(name, stock), que recibe un nombre (texto) y un stock (cantidad), agregue ambos datos como un único objeto al array catalog. Luego tiene que mostrar por consola la colección. 
+ */
+function addToCatalog(name, stock) {
+    // completar
+}
+
+
+/* TEST sample call */
+addToCatalog('honey', 20)
+
+
+
